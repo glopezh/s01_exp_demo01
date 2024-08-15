e7e019672e23620b257a0fa64dccd0cfc546661d
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-08-15 20:14:20 +0200

    first commit

---
+++ src/main/java/com/cjava/s01_exp_demo01/S01ExpDemo01Application.java
@@ -0,0 +1,14 @@
+package com.cjava.s01_exp_demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class S01ExpDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S01ExpDemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cjava/s01_exp_demo01/S01ExpDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjava.s01_exp_demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S01ExpDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
