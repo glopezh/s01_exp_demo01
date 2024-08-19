1062e36f183d6b1b28ce9e0a6e910ae8dcff8dc6
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-08-19 02:28:29 +0200

    Initial commit - cjava - sesion1 - eurekaServer

--- src/main/java/com/cjava/s01_exp_demo01/S01ExpDemo01Application.java
+++ src/main/java/com/cjava/s01_exp_demo01/S01ExpDemo01Application.java
@@ -2,8 +2,9 @@ package com.cjava.s01_exp_demo01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class S01ExpDemo01Application {
 
